ba789bb8a363eb0d78f056fae0433eafa2ee0105
ann1234555 <ann1234555@yahoo.com.tw>
2020-07-21 22:41:50 +0800

    2020/07/21 CrossOrigin

--- src/main/java/tw/com/phctw/dao/StudentRepository.java
+++ src/main/java/tw/com/phctw/dao/StudentRepository.java
@@ -10,11 +10,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import tw.com.phctw.model.Student;
 
 @RepositoryRestResource
+@CrossOrigin(origins = "http://localhost:4200")
 public interface StudentRepository extends JpaRepository<Student, Long>{
 
 	//getOne
